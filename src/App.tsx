cbe24468a1c1ea290c6dcb4116acf99039529041
Newton Duarte <newton_@hotmail.com>
2021-04-09 23:03:10 -0300

    dt Money app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import React, { useState } from "react";
+import Modal from 'react-modal';
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalClose() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModalOpen}
+      />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalClose}
+      />
+      <Dashboard />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
